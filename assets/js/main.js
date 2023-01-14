eee66d9d8092a943fe7d7ecc5af1cc8878d59b01
JorgeRamosS <thorin-70@hotmail.com>
2023-01-14 10:39:10 -0300

    cleaning 1

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,47 +1,43 @@
 // Credits Bedimcode (https://www.youtube.com/@Bedimcode)
 
-/*=============== SHOW MENU ===============*/
+/*=============== MOSTRAR MENU ===============*/
 const navMenu = document.getElementById("nav-menu"),
   navToggle = document.getElementById("nav-toggle"),
   navClose = document.getElementById("nav-close");
 
-/*===== MENU SHOW =====*/
-/* Validate if constant exists */
+/*===== MENU MOSTRAR =====*/
 if (navToggle) {
   navToggle.addEventListener("click", () => {
     navMenu.classList.add("show-menu");
   });
 }
 
-/*===== MENU HIDDEN =====*/
-/* Validate if constant exists */
+/*===== MENU OCULTAR =====*/
 if (navClose) {
   navClose.addEventListener("click", () => {
     navMenu.classList.remove("show-menu");
   });
 }
 
-/*=============== REMOVE MENU MOBILE ===============*/
+/*=============== QUITAR MENU MOBILE AL CLICKEAR SECCION ===============*/
 const navLink = document.querySelectorAll(".navigation__link");
 
 const linkAction = () => {
   const navMenu = document.getElementById("nav-menu");
-  // When we click on each nav__link, we remove the show-menu class
   navMenu.classList.remove("show-menu");
 };
 navLink.forEach((n) => n.addEventListener("click", linkAction));
 
-/*=============== CHANGE BACKGROUND HEADER ===============*/
+/*=============== CAMBIAR COLOR BACKGROUND HEADER ===============*/
 const scrollHeader = () => {
   const header = document.getElementById("header");
-  // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
   this.scrollY >= 50
     ? header.classList.add("bg-header")
     : header.classList.remove("bg-header");
 };
 window.addEventListener("scroll", scrollHeader);
 
-/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+/*=============== SECCION ACTIVA ===============*/
 const sections = document.querySelectorAll("section[id]");
 
 const scrollActive = () => {
@@ -64,10 +60,9 @@ const scrollActive = () => {
 };
 window.addEventListener("scroll", scrollActive);
 
-/*=============== SHOW SCROLL UP ===============*/
+/*=============== MOSTRAR SCROLL UP ===============*/
 const scrollUp = () => {
   const scrollUp = document.getElementById("scroll-up");
-  // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup class
   this.scrollY >= 350
     ? scrollUp.classList.add("show-scroll")
     : scrollUp.classList.remove("show-scroll");
